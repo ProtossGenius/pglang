9ca38eb3bf2d0f1ff84a11bea85d72e53225e74c
protossgenius <guyvejianglou@outlook.com>
2020-02-06 18:36:32 +0800

    ""analysis lex Ident"

---
+++ analysis/pgl_ana_lex/lex_analysis.go
@@ -0,0 +1,88 @@
+package pgl_ana_lex
+
+import (
+	"fmt"
+	"unicode"
+
+	"github.com/ProtossGenius/SureMoonNet/basis/smn_analysis"
+)
+
+const (
+	PGLA_PRODUCT_ = iota
+	PGLA_PRODUCT_IDENT
+)
+const (
+	ErrTypeNotMatch = "ErrTypeNotMatch: AnalysisReader %s, input [%s], reason %s "
+)
+
+type PglaInput struct {
+	Char rune
+}
+
+func (p *PglaInput) Copy() smn_analysis.InputItf {
+	return &PglaInput{Char: p.Char}
+}
+func read(input smn_analysis.InputItf) rune {
+	return input.(*PglaInput).Char
+}
+
+type PglaIdent struct {
+	Name string
+}
+
+func (p *PglaIdent) ProductType() int {
+	return PGLA_PRODUCT_IDENT
+}
+
+type IdentifierReader struct {
+	first  bool
+	result *PglaIdent
+}
+
+func NewLexAnalysiser() *smn_analysis.StateMachine {
+	sm := new(smn_analysis.StateMachine).Init()
+	dft := smn_analysis.NewDftStateNodeReader(sm)
+	dft.Register(&IdentifierReader{})
+	return sm
+}
+func (this *IdentifierReader) onErr(inputs, reason string) error {
+	return fmt.Errorf(ErrTypeNotMatch, "IdentifierReader", inputs, reason)
+}
+
+//only see if should stop read.
+func (this *IdentifierReader) PreRead(stateNode *smn_analysis.StateNode, input smn_analysis.InputItf) (isEnd bool, err error) {
+	char := read(input)
+	charStr := string([]rune{char})
+	if this.first {
+		if unicode.IsDigit(char) {
+			return true, this.onErr(charStr, "First char can't be Number")
+		}
+		if !unicode.IsLetter(char) && char != '_' {
+			return true, this.onErr(charStr, "Not Identifier")
+		}
+	} else {
+		if !unicode.IsDigit(char) && !unicode.IsNumber(char) && char == '_' {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+//real read. even isEnd == true the input be readed.
+func (this *IdentifierReader) Read(stateNode *smn_analysis.StateNode, input smn_analysis.InputItf) (isEnd bool, err error) {
+	char := read(input)
+	charStr := string([]rune{char})
+	this.result.Name += charStr
+	return false, nil
+}
+
+//return result
+func (this *IdentifierReader) GetProduct() smn_analysis.ProductItf {
+	return this.result
+}
+
+//let the Reader like new.  it will be call before first Read
+func (this *IdentifierReader) Clean() {
+	this.first = true
+	this.result = &PglaIdent{}
+}
